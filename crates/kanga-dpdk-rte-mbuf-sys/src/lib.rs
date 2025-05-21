6c89780fea1d8a6b618f6acdd201bc75d3ff2ce6
David Cuthbert <dacut@kanga.org>
2025-05-21 13:02:56 -0700

    Add librte_mbuf

---
+++ crates/kanga-dpdk-rte-mbuf-sys/src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unsafe_op_in_unsafe_fn)]
+#![allow(clippy::upper_case_acronyms)]
+
+pub use {kanga_dpdk_rte_eal_sys::rte_iova_t, kanga_dpdk_rte_mempool_sys::rte_mempool, libc::FILE};
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mbuf_flag() {
+        unsafe {
+            let flag = rte_get_rx_ol_flag_name(1);
+            assert!(!flag.is_null());
+        }
+    }
+}
